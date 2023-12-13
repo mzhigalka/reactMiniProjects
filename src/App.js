2b8981ab98c0f3600ec9c3c16b6be56695eeba7a
Matvey Zhigalka <mzhigalka@gmail.com>
2023-12-13 13:47:49 +0200

    firtsMiniReactApp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const countPlus = () => {
+    setCount(count + 1);
+  }
+  const countMinus = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={countMinus} className="minus">- Минус</button>
+        <button onClick={countPlus}  className="plus">Плюс +</button>
       </div>
     </div>
   );
